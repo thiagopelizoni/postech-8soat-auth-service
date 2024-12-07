b2f2c525fb87b48db3dbc7f7f3eef63e457274f1
Thiago Pelizoni <thiago.pelizoni@gmail.com>
2024-12-07 13:27:10 -0300

    Configurações do Github Actions

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "fcl-terraform-state"
-    key            = "TechChallenge/auth-service/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
+    bucket  = "fcl-terraform-state"
+    key     = "TechChallenge/auth-service/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
   }
 }
